a164f8bc617741622ca5f674be3057bff605e316
delayer2208 <Veresan72@gmail.com>
2024-06-09 19:53:44 +0200

    Finish

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-"""
-    Flask application
-"""
+"""app"""
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
-# Initialize Flask app
+
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_storage(exc):
-    """ Close the storage engine """
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
 
+
 @app.errorhandler(404)
-def page_not_found(error):
-    """ Handle 404 errors with a JSON response """
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST", '0.0.0.0')
-    port = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,80 +1,92 @@
 #!/usr/bin/python3
-"""Places and Reviews API endpoints"""
+"""places_reviews"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.place import Place
 from models.review import Review
+from datetime import datetime
+import uuid
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['GET'])
 @app_views.route('/places/<place_id>/reviews/', methods=['GET'])
 def list_reviews_of_place(place_id):
-    """Retrieves a list of all Review objects of a Place"""
-    place = storage.get(Place, place_id)
-    if not place:
+    ''' Retrieves a list of all Review objects of a Place '''
+    all_places = storage.all("Place").values()
+    place_obj = [obj.to_dict() for obj in all_places if obj.id == place_id]
+    if place_obj == []:
         abort(404)
-    reviews = [review.to_dict() for review in place.reviews]
-    return jsonify(reviews)
+    list_reviews = [obj.to_dict() for obj in storage.all("Review").values()
+                    if place_id == obj.place_id]
+    return jsonify(list_reviews)
 
 
 @app_views.route('/places/<place_id>/reviews', methods=['POST'])
 def create_review(place_id):
-    """Creates a Review for a Place"""
+    '''Creates a Review'''
     if not request.get_json():
         abort(400, 'Not a JSON')
     if 'user_id' not in request.get_json():
         abort(400, 'Missing user_id')
+    user_id = request.json['user_id']
     if 'text' not in request.get_json():
         abort(400, 'Missing text')
-
-    user_id = request.json['user_id']
-    place = storage.get(Place, place_id)
-    if not place:
+    all_places = storage.all("Place").values()
+    place_obj = [obj.to_dict() for obj in all_places if obj.id == place_id]
+    if place_obj == []:
         abort(404)
-
-    user = storage.get('User', user_id)
-    if not user:
+    all_users = storage.all("User").values()
+    user_obj = [obj.to_dict() for obj in all_users if obj.id == user_id]
+    if user_obj == []:
         abort(404)
-
-    new_review = Review(text=request.json['text'], place_id=place_id, user_id=user_id)
+    reviews = []
+    new_review = Review(text=request.json['text'], place_id=place_id,
+                        user_id=user_id)
     storage.new(new_review)
     storage.save()
-    return jsonify(new_review.to_dict()), 201
+    reviews.append(new_review.to_dict())
+    return jsonify(reviews[0]), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'])
 def get_review(review_id):
-    """Retrieves a Review object"""
-    review = storage.get(Review, review_id)
-    if not review:
+    '''Retrieves a Review object '''
+    all_reviews = storage.all("Review").values()
+    review_obj = [obj.to_dict() for obj in all_reviews if obj.id == review_id]
+    if review_obj == []:
         abort(404)
-    return jsonify(review.to_dict())
+    return jsonify(review_obj[0])
 
 
 @app_views.route('/reviews/<review_id>', methods=['DELETE'])
 def delete_review(review_id):
-    """Deletes a Review object"""
-    review = storage.get(Review, review_id)
-    if not review:
+    '''Deletes a Review object'''
+    all_reviews = storage.all("Review").values()
+    review_obj = [obj.to_dict() for obj in all_reviews if obj.id == review_id]
+    if review_obj == []:
         abort(404)
-    storage.delete(review)
-    storage.save()
+    review_obj.remove(review_obj[0])
+    for obj in all_reviews:
+        if obj.id == review_id:
+            storage.delete(obj)
+            storage.save()
     return jsonify({}), 200
 
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'])
-def update_review(review_id):
-    """Updates a Review object"""
-    review = storage.get(Review, review_id)
-    if not review:
+def updates_review(review_id):
+    '''Updates a Review object'''
+    all_reviews = storage.all("Review").values()
+    review_obj = [obj.to_dict() for obj in all_reviews if obj.id == review_id]
+    if review_obj == []:
         abort(404)
     if not request.get_json():
         abort(400, 'Not a JSON')
-
-    update_data = request.get_json()
-    for key, value in update_data.items():
-        if key in ['text']:
-            setattr(review, key, value)
-    storage.save()
-    return jsonify(review.to_dict()), 200
+    if 'text' in request.get_json():
+        review_obj[0]['text'] = request.json['text']
+        for obj in all_reviews:
+            if obj.id == review_id:
+                obj.text = request.json['text']
+        storage.save()
+    return jsonify(review_obj[0]), 200
