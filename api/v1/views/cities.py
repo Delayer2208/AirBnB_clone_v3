052cc4655fa9e6e9bf2298bf99fe1e700975b24e
delayer2208 <Veresan72@gmail.com>
2024-06-10 23:54:37 +0200

    Finish

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,66 +1,84 @@
 #!/usr/bin/python3
-"""Handles all RESTful API actions for City objects"""
+"""cities"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.city import City
 from models.state import State
+from datetime import datetime
+import uuid
+
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities/', methods=['GET'])
 def list_cities_of_state(state_id):
-    """Retrieves all City objects for a given state"""
-    state = storage.get("State", state_id)
-    if not state:
+    '''Retrieves a list of all City objects'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
-    cities = [city.to_dict() for city in state.cities]
-    return jsonify(cities)
+    list_cities = [obj.to_dict() for obj in storage.all("City").values()
+                   if state_id == obj.state_id]
+    return jsonify(list_cities)
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
+@app_views.route('/states/<state_id>/cities/', methods=['POST'])
 def create_city(state_id):
-    """Creates a new City object for a given state"""
-    state = storage.get("State", state_id)
-    if not state:
-        abort(404)
-    if not request.json:
+    '''Creates a City'''
+    if not request.get_json():
         abort(400, 'Not a JSON')
-    if 'name' not in request.json:
+    if 'name' not in request.get_json():
         abort(400, 'Missing name')
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
+        abort(404)
+    cities = []
     new_city = City(name=request.json['name'], state_id=state_id)
     storage.new(new_city)
     storage.save()
-    return jsonify(new_city.to_dict()), 201
+    cities.append(new_city.to_dict())
+    return jsonify(cities[0]), 201
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'])
 def get_city(city_id):
-    """Retrieves a specific City object"""
-    city = storage.get("City", city_id)
-    if not city:
+    '''Retrieves a City object'''
+    all_cities = storage.all("City").values()
+    city_obj = [obj.to_dict() for obj in all_cities if obj.id == city_id]
+    if city_obj == []:
         abort(404)
-    return jsonify(city.to_dict())
+    return jsonify(city_obj[0])
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
-    """Deletes a specific City object"""
-    city = storage.get("City", city_id)
-    if not city:
+    '''Deletes a City object'''
+    all_cities = storage.all("City").values()
+    city_obj = [obj.to_dict() for obj in all_cities if obj.id == city_id]
+    if city_obj == []:
         abort(404)
-    storage.delete(city)
-    storage.save()
+    city_obj.remove(city_obj[0])
+    for obj in all_cities:
+        if obj.id == city_id:
+            storage.delete(obj)
+            storage.save()
     return jsonify({}), 200
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'])
-def update_city(city_id):
-    """Updates a specific City object"""
-    city = storage.get("City", city_id)
-    if not city:
+def updates_city(city_id):
+    '''Updates a City object'''
+    all_cities = storage.all("City").values()
+    city_obj = [obj.to_dict() for obj in all_cities if obj.id == city_id]
+    if city_obj == []:
         abort(404)
-    if not request.json:
+    if not request.get_json():
         abort(400, 'Not a JSON')
-    if 'name' in request.json:
-        city.name = request.json['name']
+    city_obj[0]['name'] = request.json['name']
+    for obj in all_cities:
+        if obj.id == city_id:
+            obj.name = request.json['name']
     storage.save()
-    return jsonify(city.to_dict()), 200
+    return jsonify(city_obj[0]), 200
